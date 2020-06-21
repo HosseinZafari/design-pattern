afb8676bd424438f9aa17451d969e7c73f40b111
Hossein Zafari <ho3einzafari@gmail.com>
2020-06-21 23:30:04 +0430

    Add Helper Class -> Log

---
+++ src/util/Log.java
@@ -0,0 +1,9 @@
+package util;
+
+public class Log {
+
+  public static void d(Object object){
+    System.out.println("Debug: ThreadId: #" + Thread.currentThread().getId() + "\t|" + object.toString());
+  }
+
+}
