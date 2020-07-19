e3cce76bd91f2bec32b0fca9e067f5af535d02a4
Hossein <ho3einzafari@gmail.com>
2020-07-19 23:59:56 +0430

    add Comment For factory

--- src/creational/factory_method/example1/Stage.java
+++ src/creational/factory_method/example1/Stage.java
@@ -3,8 +3,9 @@ package creational.factory_method.example1;
 public class Stage {
 
   public static void main(String[] args) {
-      ProccessingCpu cpu = ProccessingFactory.getProccessingCpu(ProccessingFactory.GIF);
-      cpu.proccess();
+    // An Example For Test Design Pattern Factory Method
+    ProccessingCpu cpu = ProccessingFactory.getProccessingCpu(ProccessingFactory.GIF);
+    cpu.proccess();
   }
 
 
